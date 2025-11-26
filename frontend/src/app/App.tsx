59141db1fb39e0b46440874c99dae7c7d05ddd13
ZudaR <zudin_daniil@mail.ru>
2025-11-26 21:03:14 +0300

    feat(app): wire providers and router

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,18 @@
+import { AppRouter } from "./router";
+import { QueryProvider } from "./providers/QueryProvider";
+import { AuthProvider } from "./providers/AuthProvider";
+import { RootLayout } from "./layout/RootLayout";
+
+function App() {
+	return(
+		<QueryProvider>
+			<AuthProvider>
+				<RootLayout>
+					<AppRouter />
+				</RootLayout>
+			</AuthProvider>
+		</QueryProvider>
+	);
+}
+
+export default App;
