a72ce8bedbcacbffdf4db7ba6fb1bc6d5704e33c
ZudaR <d.zudin@alarmtrade.ru>
2025-11-26 13:35:28 +0300

    feat(app): setup routes and router

---
+++ frontend/src/app/routes.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+	login: '/login',
+	projects: '/projects',
+	projectTasks: (projectId = ':projectId') => `/projects/${projectId}/tasks`,
+	projectBoard: (projectId = ':projectId') => `/projects/${projectId}/board`,
+	calendar: '/calendar',
+} as const;
